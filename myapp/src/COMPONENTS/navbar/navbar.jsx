c9b1c981271a928d1c60b234a93c6bc2c0ba065e
vinayakjain <vinaaykjain388@gmail.com>
2024-02-22 00:51:19 +0530

    added movie suggestion dropdown for searching movie

--- myapp/src/COMPONENTS/navbar/navbar.jsx
+++ myapp/src/COMPONENTS/navbar/navbar.jsx
@@ -1,18 +1,34 @@
-import './navbar.css'
-function Navbar(){
-    return (
-        <>
-          <div className="navbar-wrapper">
-            <div>
-                Movie Base
-            </div>
-            <div className="search-bar">
-                <input type="text" placeholder='search your movie here'/>
-            </div>
-            <div>theme</div>
+import { useRef } from "react";
+import "./navbar.css";
+function Navbar() {
+  const resultListRef = useRef(null);
+  return (
+    <>
+      <div className="navbar-wrapper">
+        <div>Movie Base</div>
+        <div className="search-bar">
+          <input
+            type="text"
+            placeholder="search your movie here"
+            id="movie-search-input"
+            onFocus={() => {
+              resultListRef.current.style.display = "block";
+            }}
+            onBlur={() => {
+              resultListRef.current.style.display = "none";
+            }}
+          />
+          <div className="movies-suggestion" ref={resultListRef}>
+            <span className="movie-result">result 1</span>
+            <span className="movie-result">result2</span>
+            <span className="movie-result">result3</span>
+            <span className="movie-result">result4</span>
           </div>
-        </>
-    )
+        </div>
+        <div>theme</div>
+      </div>
+    </>
+  );
 }
 
 export default Navbar;
